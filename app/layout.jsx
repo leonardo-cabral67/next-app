c84ff4fb9e19d5f31b921d8fedf42de65ccc2e74
Leonardo Cabral <80075113+leonardo-cabral67@users.noreply.github.com>
2023-03-07 20:28:34 +0000

    feat: add font montSerrat to index layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,16 @@
 import './globals.css' 
+import {Montserrat} from '@next/font/google'
+
+const montSerrat = Montserrat({
+    weight: ["400", "700"],
+    subsets: ["latin"],
+    variables: "--font-montserrat"
+})
 
 export default function RootLayout({children}) {
     return (
         <html lang="pt-br">
-            <body>
+            <body className={`${montSerrat.className}`}>
                 {children}
             </body>
         </html>
